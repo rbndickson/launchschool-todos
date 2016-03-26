8c30708bfc903d456c441b7bb9a5c67857139d61
Robin Dickson <robindickson@gmail.com>
2016-03-26 10:10:30 -0600

    Add validations and flash messages when adding list

--- todo.rb
+++ todo.rb
@@ -24,7 +24,25 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+def error_for_list_name(name)
+  if session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  elsif !(1..100).cover?(name.length)
+    "List name must be between 1 and 100 characters."
+  end
+end
+
 post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect "/lists"
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: params[:list_name], todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
 end
