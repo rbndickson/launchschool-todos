08f52822bc1ac6124487d39352aed2e1fdcdfca1
Robin Dickson <robindickson@gmail.com>
2016-03-27 19:51:19 -0600

    Add deleting of todo items

--- todo.rb
+++ todo.rb
@@ -108,3 +108,13 @@ post "/lists/:list_id/todos" do
     redirect "/lists/#{@list_id}"
   end
 end
+
+post "/lists/:list_id/todos/:todo_id/destroy" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:todo_id].to_i
+  @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
