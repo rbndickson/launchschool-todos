1e87a767649ce04313f83468cd5351c2c5c120c6
Robin Dickson <robindickson@gmail.com>
2016-03-27 19:36:37 -0600

    Add todos for eash list

--- todo.rb
+++ todo.rb
@@ -50,8 +50,8 @@ post '/lists' do
 end
 
 get '/lists/:id' do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
 
   erb :list, layout: :layout
 end
@@ -65,7 +65,7 @@ end
 
 post '/lists/:id' do
   list_name = params[:list_name].strip
-  id = id.to_i
+  id = params[:id].to_i
   @list = session[:lists][id]
 
   error = error_for_list_name(list_name)
@@ -85,4 +85,26 @@ post "/lists/:id/destroy" do
   session[:lists].delete_at(id)
   session[:success] = "The list has been deleted."
   redirect "/lists"
+end
+
+def error_for_todo(name)
+  if !(1..100).cover?(name.length)
+    'Todo must be between 1 and 100 characters.'
+  end
+end
+
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << { name: text, completed: false }
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
+  end
 end
