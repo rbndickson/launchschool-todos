38b634c7e5e67b997b0dc3ca200feefefe9399bd
Robin Dickson <robindickson@gmail.com>
2016-03-28 22:18:53 -0600

    Add Javascript delete confirmation dialog

---
+++ public/javascripts/application.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure?");
+    if (ok) {
+      this.submit();
+    }
+
+  });
+
+});
